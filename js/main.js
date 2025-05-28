017c3793283cd86a3bfef16b64d672e15b59ca3c
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-28 15:55:08 +0200

    Fix FizzBuzz output logic to correctly display "FizzBuzz" for numbers that are multiples of both 3 and 5

--- js/main.js
+++ js/main.js
@@ -42,11 +42,13 @@ Consigli del giorno:
 // lo riscrivo inserendo le informazioni richieste sui multipli di 3 e i multipli di 5
 
 for(let i = 1; i <= 100; i++ ){
-        console.log( i );
         if ( i % 3 == 0 ) {
-            console.log( i, "Fizz");
+            console.log( i, "Fizz" );
         }
         else if ( i % 5 == 0 ) {
-            console.log( i, "Buzz");            
+            console.log( i, "Buzz" ); 
+        }
+        else if ( ( i % 3 != 0 ) && ( i % 5 != 0) ) {
+            console.log( i, "FizzBuzz" )
         }
     }
