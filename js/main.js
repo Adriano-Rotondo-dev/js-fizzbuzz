272f855e060029cf55c8d19a385a20d324e15d8b
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-28 17:41:09 +0200

    Fix formatting in FizzBuzz logic for consistency

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ Consigli del giorno:
 //Inizio For Loop
 for (let i = 1; i <= 100; i++) {
     // inserisco la condizione per cui ai numeri multipli sia di 3 che di 5 sia associata la stampa "FizzBuzz"
-    if (i % 3 == 0 && i % 5 == 0) {  // inizializzo la condizione per i numeri multipli di entrambi
+    if (i % 3 == 0 && i % 5 == 0) { // inizializzo la condizione per i numeri multipli di entrambi
         console.log(i, "FizzBuzz") // stampa del FizzBuzz sui numeri multipli sia di 3 che di 5 
     }
     // inserisco la condizione del modulo % 3
